9817223976ff591617dafd64e485997923f8e2d8
Cristian Fildan <cristian.fildan@nlc.health>
2023-03-03 12:01:13 +0100

    Eliminated files from model and removed train_before

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from helper import Model
 from helper import Auxiliary
 
 
-df = Model.get_csv(r'C:\Users\20212387\OneDrive - TU Eindhoven\Documents\Y2\DBL process mining\ProcessMining\Model\BPI_Challenge_2012.csv')
-Auxiliary.train_test_split(df)
+df = Model.get_csv(r'Model\BPI_Challenge_2012.csv')
+#Auxiliary.train_test_split(df)
 
 
