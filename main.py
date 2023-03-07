22ed10f6fa106c1a86c62f07528d715dcf6bb50b
CasperRoes <c.b.roes@student.tue.nl>
2023-03-07 09:51:45 +0100

    Start of tt split graph

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #The model instance goes here
 from helper import Model
 from helper import Auxiliary
+from helper import Graphs
 
 
 df = Model.get_csv(r'Model\BPI_Challenge_2012.csv')
 #Auxiliary.train_test_split(df)
-
 
